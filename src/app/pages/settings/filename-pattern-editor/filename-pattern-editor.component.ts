6dc4907285ad7442b478e45b16f3a3e6ab689347
Claudio Nicora <claudio.nicora@gmail.com>
2023-11-05 11:20:27 +0100

    Improved style of settings page

--- src/app/pages/settings/filename-pattern-editor/filename-pattern-editor.component.ts
+++ src/app/pages/settings/filename-pattern-editor/filename-pattern-editor.component.ts
@@ -1,9 +1,10 @@
+import { Subscription } from 'rxjs';
 import { FILENAME_PATTERN_DEFAULT, Recording } from 'src/app/models/recording';
 import { RecordingsService } from 'src/app/services/recordings.service';
 import { SettingsService } from 'src/app/services/settings.service';
 import { AndroidSAF, ErrorCode } from 'src/plugins/capacitorandroidsaf';
 import { Component, ElementRef, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-filename-pattern-editor',
@@ -15,21 +16,29 @@ export class FilenamePatternEditorComponent implements OnInit {
   protected testFilename = '';
   protected testResult = '';
 
-  @Input({ required: true })
-  pattern!: string;
-
+  @Input({ required: true }) pattern!: string;
   public onConfirm?: (pattern: string) => Promise<void>;
+
+  private backSub?: Subscription;
 
   constructor(
     protected mc: ModalController,
+    protected platform: Platform,
     protected ref: ElementRef<HTMLIonModalElement>,
     protected settings: SettingsService,
     protected recordingsSvc: RecordingsService,
-  ) { }
+  ) {
+    // subscribe to hardware back button events
+    this.backSub = this.platform.backButton.subscribeWithPriority(10, () => this.cancel());
+  }
 
   ngOnInit() {
     // set own class
     this.ref.nativeElement.parentElement?.classList.add('tag-editor');
+  }
+
+  async ionViewWillLeave() {
+    this.backSub?.unsubscribe();
   }
 
   default() {
