e230c9f7aa86f7ed7bbd13291567e090716b8949
Claudio Nicora <claudio.nicora@gmail.com>
2024-01-21 14:15:03 +0100

    Added build script to check Android versionCode and avoid errors

---
+++ scripts/check-android-version.js
@@ -0,0 +1,25 @@
+/**
+ * Create a bunch of test  files
+ */
+const fs = require('fs');
+const process = require('process');
+const versionFile = 'android/app/build.gradle';
+
+// read build file content
+const content = fs.readFileSync(versionFile).toString();
+
+// extract versions
+const versionName = /^\s+versionName\s+"(?<ver>.*?)"$/gm.exec(content)?.groups['ver'];
+console.log('Found versionName:', versionName);
+const versionCode = /^\s+versionCode\s+(?<ver>.*?)$/gm.exec(content)?.groups['ver'];
+console.log('Found versionCode:', versionCode);
+
+// build expected versionCode
+const expectedVersionCode = versionName.split('.').map(p => p.padStart(3, '0')).join('');
+
+// test
+if (versionCode !== expectedVersionCode) {
+  console.log('Expected version code:', expectedVersionCode);
+  console.error('[ERROR] Please fix "versionCode" it in file:', versionFile);
+  process.exit(1);
+}
