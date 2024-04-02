ec95a513176d70ed8d02c987035434db3f60beff
Claudio Nicora <claudio.nicora@gmail.com>
2024-04-02 23:17:59 +0200

    Fixed language check dev script

--- scripts/check-i18n-files.ts
+++ scripts/check-i18n-files.ts
@@ -7,17 +7,19 @@ import * as readline from 'readline';
 import { Culture, CultureContent, I18nKey } from 'src/app/services/i18n.service';
 
 // constants
+const SHOW_DETAILS = false;
 const basePath = path.resolve(path.dirname(__filename) + '/..');
 const langDir = basePath + '/src/assets/i18n';
 const configFile = '_cultures.json';
 
 // fields
-let cultureIDs: string[];
+let activeCultureIDs: string[];
+let allCultureIDs: string[];
 let srcCultureID: string;
 let srcCultureContent: CultureContent;
 let srcCultureKeyCount: number;
 let contents: { [key:string]: CultureContent };
-let results: { [key:string]: CheckResult };
+let results: CheckResult[];
 
 // readline sync
 async function readLineAsync(message: string) {
@@ -37,7 +39,7 @@ async function readLineAsync(message: string) {
  * Results class
  */
 class CheckResult {
-  missing: I18nKey[] = [];
+  cid: string = '';
   extra: I18nKey[] = [];
   untranslated: I18nKey[] = [];
 }
@@ -64,32 +66,15 @@ function checkCultureContent(cid: string): CheckResult {
   const res = new CheckResult();
   const cultureContent = contents[cid];
 
-  // missing keys
-  res.missing = Object.keys(srcCultureContent).filter(k => cultureContent[<I18nKey>k] === undefined) as I18nKey[];
+  res.cid = cid;
 
   // extra keys
   res.extra = Object.keys(cultureContent).filter(k => srcCultureContent[<I18nKey>k] === undefined) as I18nKey[];
 
   // untranslated keys
-  res.untranslated = Object.keys(srcCultureContent).filter(k => cultureContent[<I18nKey>k] !== undefined && cultureContent[<I18nKey>k].trim().length === 0) as I18nKey[];
+  res.untranslated = Object.keys(srcCultureContent).filter(k => cultureContent[<I18nKey>k] === undefined || cultureContent[<I18nKey>k].trim().length === 0) as I18nKey[];
 
   return res;
-}
-
-/**
- * Fix culture files
- */
-function fixFiles() {
-  Object.entries(results).forEach(([cid, res]) => {
-    if (res.missing.length) {
-      console.log(`Fixing culture ${cid}`);
-      const content = contents[cid];
-      // add missing keys
-      res.missing.forEach(k => content[<I18nKey>k] = '');
-      // save lang file
-      saveCultureContent(cid, content);
-    }
-  });
 }
 
 // main function
@@ -97,27 +82,34 @@ async function main() {
 
   console.log(`Reading config file ${configFile}...`);
 
+  // load available culture files (active or not)
+  allCultureIDs = fs.readdirSync(langDir)
+    .filter(i => /\.json$/.test(i))
+    .filter(i => i !== configFile)
+    .map(i => path.parse(i).name);
+
   // load defined cultures (except for srcCulture, that will be "shifted" below)
-  cultureIDs = (<Culture[]>JSON.parse(fs.readFileSync(`${langDir}/${configFile}`).toString())).map(c => c.id);
-  if (!cultureIDs.length) {
+  activeCultureIDs = (<Culture[]>JSON.parse(fs.readFileSync(`${langDir}/${configFile}`).toString())).map(c => c.id);
+  if (!allCultureIDs.length) {
     console.error('No cultures defined!');
     process.exit(1);
   }
 
   // src culture is the first one defined in cultures array
-  srcCultureID = cultureIDs.shift()!;
+  srcCultureID = activeCultureIDs.shift()!;
   srcCultureContent = loadCultureContent(srcCultureID);
   srcCultureKeyCount = Object.keys(srcCultureContent).length;
 
   // load culture contents
   contents = Object.fromEntries(
-    cultureIDs.map(cid => [cid, loadCultureContent(cid)])
+    allCultureIDs.map(cid => [cid, loadCultureContent(cid)])
   );
 
   // init check results
-  results = Object.fromEntries(
-    cultureIDs.map(cid => [cid, checkCultureContent(cid)])
-  );
+  results = allCultureIDs
+    .map(cid => checkCultureContent(cid))
+    .sort((a, b) => a.untranslated.length - b.untranslated.length || a.cid?.localeCompare(b.cid))
+  ;
 
   // print statistics
   console.log('----------------------------------------------------------------------------------------');
@@ -129,30 +121,21 @@ async function main() {
   console.log();
 
   console.log('Check results:');
-  let totalMissing = 0;
-  for (const cid in results) {
-    const res = results[cid];
-    console.log(`[${cid}]: Progress: ${Math.floor((srcCultureKeyCount-res.untranslated.length) / srcCultureKeyCount * 100)}%, Untranslated: ${res.untranslated.length}, Missing: ${res.missing.length}, Extra: ${res.extra.length}`);
-    if (res.missing.length) {
-      totalMissing += res.missing.length;
-      console.error(`  Missing: ${res.missing.join(', ')}`);
-    }
+  for (const res of results) {
+    console.log(
+      ('['+res.cid+']:').padEnd(10, ' '),
+      `Progress: ${Math.floor((srcCultureKeyCount-res.untranslated.length) / srcCultureKeyCount * 100).toString().padStart(4, ' ')}%,`,
+      `Untranslated: ${res.untranslated.length.toString().padStart(4, ' ')},`,
+      `Active: ${res.cid === srcCultureID || activeCultureIDs.includes(res.cid)}`,
+    );
     if (res.extra.length) {
       console.error(`  Extra: ${res.extra.join(', ')}`);
     }
-    if (res.untranslated.length) {
+    if (SHOW_DETAILS && res.untranslated.length) {
       console.warn(`  Untranslated: ${res.untranslated.join(', ')}`);
     }
   }
-
-  // ask permission to fix files
-  if (totalMissing) {
-    console.log();
-    var choice = (await readLineAsync('One or more files has missing keys. Do you want to fix them (y/N)? ')).toLowerCase();
-    if (choice === 'y') {
-      fixFiles();
-    }
-  }
+  console.log();
 
 }
 
