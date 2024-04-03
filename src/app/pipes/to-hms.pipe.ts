5ab3bbdfb84e270ab0084d09dd3ea69c12f9f190
Claudio Nicora <claudio.nicora@gmail.com>
2024-04-03 20:01:45 +0200

    Fixed toHmsPipe to round seconds decimals

--- src/app/pipes/to-hms.pipe.ts
+++ src/app/pipes/to-hms.pipe.ts
@@ -9,7 +9,7 @@ export class ToHmsPipe implements PipeTransform {
   transform(totalSeconds: number): string {
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
+    const seconds = Math.round(totalSeconds % 60);
     let result = `${minutes
       .toString()
       .padStart(1, '0')}:${seconds.toString().padStart(2, '0')}`;
