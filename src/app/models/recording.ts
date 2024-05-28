f561cafc829fc3906d3c2190dda68b6ad0a256c4
Maciej Łoziński <maciej@robotix-lozinski.pl>
2024-05-28 13:28:00 +0200

    Fix accidental text insertion

--- src/app/models/recording.ts
+++ src/app/models/recording.ts
@@ -7,18 +7,7 @@ export const FILENAME_PATTERN_SUPPORTED_VARS = [
   'date',
   'date:year',
   'date:year2',
-  'date:month',**Is your feature request related to a problem? Please describe.**
-A clear and concise description of what the problem is. Ex. I'm always frustrated when [...]
-
-**Describe the solution you'd like**
-A clear and concise description of what you want to happen.
-
-**Describe alternatives you've considered**
-A clear and concise description of any alternative solutions or features you've considered.
-
-**Additional context**
-Add any other context or screenshots about the feature request here.
-
+  'date:month',
   'date:day',
   'date:hours',
   'date:minutes',
