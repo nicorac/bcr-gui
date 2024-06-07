bcf9c37a1567dbe0da176dcd34f0c447f841d6cb
Claudio Nicora <claudio.nicora@gmail.com>
2024-06-07 20:43:38 +0200

    Added color to call direction icons (closes #126)

--- src/app/components/call-icon/call-icon.component.ts
+++ src/app/components/call-icon/call-icon.component.ts
@@ -1,15 +1,17 @@
 import { CallDirection } from 'src/app/models/BcrRecordingMetadata';
 import { NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-call-icon',
   templateUrl: './call-icon.component.html',
+  styleUrl: './call-icon.component.scss',
   standalone: true,
   imports: [ NgIf ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CallIconComponent {
 
-  @Input({ required: true }) direction: CallDirection = '';
+  public direction = input.required<CallDirection>();
 
 }
