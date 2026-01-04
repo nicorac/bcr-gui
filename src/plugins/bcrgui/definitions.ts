812ada492299026a7e39577d17fd8ad4a8a310a0
Victor Tseng <palatis@gmail.com>
2026-01-04 01:16:04 +0100

    Integrated PR#219 from @palatis (squashed) - show specific backend settings based on user selection - add an entry in settings page to open BCR settings directly - implement "launchActivity()" for BcrGui plugin

--- src/plugins/bcrgui/definitions.ts
+++ src/plugins/bcrgui/definitions.ts
@@ -6,4 +6,8 @@ export interface BcrGui {
    */
   createOrEditContact(options: { displayName?: string, phoneNumber?: string }): Promise<{ contactUri: string, displayName: string }>;
 
+  /**
+   * Launch an Android activity from BCR app
+   */
+  launchActivity(options: { component: string }): Promise<void>;                                                      
 }
